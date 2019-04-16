c7a1f94548acb801b77d7970081684f19324c52f
Aiqz <aqz1995@163.com>
2019-04-16 09:19:06 +0800

    stein

---
+++ svgd.py
@@ -0,0 +1,56 @@
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+
+class SVGD():
+
+    def __init__(self):
+        pass
+    
+    def svgd_kernel(self, theta, h=-1):
+        sq_dist = pdist(theta)
+        pairwise_dists = squareform(sq_dist)**2   # Matrix of ||x-x'||^2, symmetry
+        if h < 0: # if h < 0, using median trick
+            # np.median--Median of the matrix
+            h = np.median(pairwise_dists)  
+            h = np.sqrt(0.5 * h / np.log(theta.shape[0]+1))
+
+        # compute the RBF kernel
+        Kxy = np.exp(-pairwise_dists / h**2 / 2)
+        dxkxy = -np.matmul(Kxy, theta)
+        sumkxy = np.sum(Kxy, axis=1)
+        for i in range(theta.shape[1]):
+            dxkxy[:, i] = dxkxy[:, i] + np.multiply(theta[:, i], sumkxy)
+        dxkxy = dxkxy / (h**2)
+        # Kxy:Matrix of RBF kernel
+        # dxkxy:Derivative of kernel
+        return (Kxy, dxkxy)
+ 
+    def update(self, x0, lnprob, n_iter = 1000, stepsize = 1e-3, bandwidth = -1, alpha = 0.9, debug = True):
+        # Check input
+        if x0 is None or lnprob is None:
+            raise ValueError('x0 or lnprob cannot be None!')
+        
+        theta = np.copy(x0) 
+        
+        # adagrad with momentum
+        fudge_factor = 1e-6
+        historical_grad = 0
+        for iter in range(n_iter):
+            if debug and (iter+1) % 10 == 0:
+                print ('iter ' + str(iter+1))
+            
+            lnpgrad = lnprob(theta)
+            # calculating the kernel matrix
+            kxy, dxkxy = self.svgd_kernel(theta, h=-1)
+            grad_theta = (np.matmul(kxy, lnpgrad) + dxkxy) / x0.shape[0]  
+            
+            # Adagrad
+            if iter == 0:
+                historical_grad = historical_grad + grad_theta ** 2
+            else:
+                historical_grad = alpha * historical_grad + (1 - alpha) * (grad_theta ** 2)
+            adj_grad = np.divide(grad_theta, fudge_factor+np.sqrt(historical_grad))
+            theta = theta + stepsize * adj_grad 
+            
+        return theta
